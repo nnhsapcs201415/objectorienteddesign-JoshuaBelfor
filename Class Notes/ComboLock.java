7476a04503c4741bdc01b2603cce76e3e98c7c3e
JoshuaBelfor <degjeb2@gmail.com>
2017-01-26 15:06:39 -0800

    Created ComboLock class
    class ComboLock is designed to simulate a locker lock and entering its
    combination

---
+++ Class Notes/ComboLock.java
@@ -0,0 +1,38 @@
+
+/**
+ * Write a description of class ComboLock here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ComboLock
+{
+   public ComboLock (int secret1, int secret2, int secret3)
+   {
+    
+   }
+   
+   public void reset()
+   {
+    
+   }
+   
+   public void turnLeft(int ticks)
+   {
+    
+   }
+   
+   public void turnRight( int ticks )
+   {
+    
+   }
+   
+   public boolean open()
+   
+   {
+       boolean isOpen = true;
+       
+       return isOpen;
+   }
+}
+   
