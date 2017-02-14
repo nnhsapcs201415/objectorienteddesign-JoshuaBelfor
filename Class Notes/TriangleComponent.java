922bc876ec5a245cef8adac1456f6dc1305f238e
JoshuaBelfor <degjeb2@gmail.com>
2017-02-14 15:10:00 +0000

    ffdfd

--- Class Notes/TriangleComponent.java
+++ Class Notes/TriangleComponent.java
@@ -1,39 +1,46 @@
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Rectangle;
-import javax.swing.JComponent;/**
+import javax.swing.JComponent;
+/**
 This component displays a rectangle that can be moved.
 8*/
 public class TriangleComponent extends JComponent
 {
-    private static final int BOX_X = 100;
-    private static final int BOX_Y = 100;
-    private static final int BOX_WIDTH = 20;
-    private static final int BOX_HEIGHT = 30;
-
-    private Rectangle box;
+    private int point;
+    
+    private int[][] points;
 
     public TriangleComponent()
     {
-        // The rectangle that the paintComponent method draws
-        box = new Rectangle(BOX_X, BOX_Y, BOX_WIDTH, BOX_HEIGHT);
+        this.point = -1;
+        this.points = new int[3][2];
     }
 
     public void paintComponent(Graphics g)
     {
         Graphics2D g2 = (Graphics2D) g;
-        g2.draw(box);
+        if (point >= 0 )
+        {
+            
+        }
+        
     }
 
-    /**
-    Moves the rectangle to the given location.
-    @param x the x-position of the new location
-    @param y the y-position of the new location
-     */
-    public void moveRectangleTo(int x, int y)
+    public void moveTriangeTo(int x, int y)
     {
-        box.setLocation(x, y);
-        repaint();
+        point++;
+        if (point < 3)
+        {
+            int[] position = x,y
+            points[point] = position;
+            
+        }
+        else if (point == 3)
+        {
+            this.points = int[3][2];
+            this.point = -1;
+        
     }
     
 }
